6068fdfe46e2a1247b4f1b5791084448a8f75465
Steven Siddoway <scsiddoway@gmail.com>
2016-03-23 18:17:47 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = require('./server/app.js');
+var user = require('./user.js');
+
+/// user.js example
+
+var app = require('./server/app.js');
+
+app.get('/user/create', function(req, res) {
+
+})
